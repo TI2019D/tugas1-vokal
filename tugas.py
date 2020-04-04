b3f49cbc5b050852f671c5fb4f436b306e61a5e1
Muhammad Fahmi Rasyid <ufarasfa@gmail.com>
2020-04-04 13:31:29 +0700

    Create tugas.py

---
+++ tugas.py
@@ -0,0 +1,30 @@
+
+class HitungVokal
+	
+	A = 0
+	U = 0
+	I = 0
+	E = 0
+	O = 0
+
+	def Hitung(self, teks):
+
+		# tulis implementasi di sini.
+
+		pass # hapus pass.
+		
+
+	def Tampilkan(self):
+		print(f"A={self.A}, I={self.I}, U={self.U}, E={self.E}, O={self.O}")
+		
+
+
+
+a = input("Masukkan Teks:")
+
+vokal = HitungVokal()
+
+vokal.Hitung(a)
+
+vokal.Tampilkan()
+
